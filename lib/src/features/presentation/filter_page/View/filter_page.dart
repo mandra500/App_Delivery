548525c45d02c55c8cd835e0eb27ade7e7fd3cf9
RAY BACA <Mandra500>
2023-05-24 17:02:23 -0500

    se agregaron nuevas paginas

--- lib/src/features/presentation/filter_page/View/filter_page.dart
+++ lib/src/features/presentation/filter_page/View/filter_page.dart
@@ -1,6 +1,10 @@
 import 'package:app_restaurant/src/colors/colors.dart';
 import 'package:app_restaurant/src/features/presentation/commons_widgets/header_text.dart';
+import 'package:app_restaurant/src/features/presentation/filter_page/View/customsWidgets/categoria_filters.dart';
+import 'package:app_restaurant/src/features/presentation/filter_page/View/customsWidgets/price_filter.dart';
 import 'package:flutter/material.dart';
+
+import 'customsWidgets/lista_checked.dart';
 
 class FilterPage extends StatefulWidget {
   const FilterPage({Key? key}) : super(key: key);
@@ -10,14 +14,108 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
+  //ordenado por...
+  bool loMasVendido = false;
+  bool precioAltoABajo = false;
+  bool precioBajoAAlto = false;
+  bool masPopular = false;
+
+  //
+  //aquí pueden ir otros filtros
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(context),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin:
+                      const EdgeInsets.only(top: 5.0, bottom: 5.0, left: 15.0),
+                  child: headerText(
+                    texto: 'Categorias',
+                    color: gris,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17.0,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: const CusinesFilter(),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin:
+                      const EdgeInsets.only(top: 5.0, bottom: 5.0, left: 15.0),
+                  child: headerText(
+                    texto: 'ORDENAR POR',
+                    color: gris,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17.0,
+                  ),
+                ),
+                _sortByContainer(),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin:
+                      const EdgeInsets.only(top: 5.0, bottom: 5.0, left: 15.0),
+                  child: headerText(
+                    texto: 'PRECIOS',
+                    color: gris,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 17.0)
+                  ),
+                  const PrecioFilter()
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  AppBar _appBar(BuildContext context) {
+  Widget _sortByContainer() {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          children: [
+            ListTileOrder(
+              texto: 'Lo más vendido',
+              isActive: loMasVendido,
+              func: () {
+                setState(() => loMasVendido = !loMasVendido);
+              },
+            ),
+            ListTileOrder(
+              texto: 'Precio del mas Alto al mas Bajo',
+              isActive: precioAltoABajo,
+              func: () {
+                setState(() => precioAltoABajo = !precioAltoABajo);
+              },
+            ),
+            ListTileOrder(
+              texto: 'Precio del mas Bajo al mas Alto',
+              isActive: precioBajoAAlto,
+              func: () {
+                setState(() => precioBajoAAlto = !precioBajoAAlto);
+              },
+            ),
+            ListTileOrder(
+              texto: 'Los más Populares',
+              isActive: masPopular,
+              func: () {
+                setState(() => masPopular = !masPopular);
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+AppBar _appBar(BuildContext context) {
   return AppBar(
     elevation: 0.5,
     backgroundColor: Colors.white,
@@ -36,16 +134,20 @@ class _FilterPageState extends State<FilterPage> {
       ),
     ),
     actions: [
-      Container(
-        padding: const EdgeInsets.only(top: 20, right: 10.0),
-        child: headerText(
-          texto: 'Enviar',
-          color: orange,
-          fontWeight: FontWeight.w500,
-          fontSize: 17.0,
+      GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          padding: const EdgeInsets.only(top: 20, right: 10.0),
+          child: headerText(
+            texto: 'Enviar',
+            color: orange,
+            fontWeight: FontWeight.w500,
+            fontSize: 17.0,
+          ),
         ),
       ),
     ],
   );
-}
 }
